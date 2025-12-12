abb80a9aee3afaf75c524daa0c2820eb09693f77
Jonatt-001 <1jonatttech@gmail.com>
2025-12-12 08:08:43 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  generateBuildId: async () => {
+    return "build-" + Date.now().toString(); // forces browser to load fresh JS
+  },
+};
 
 export default nextConfig;
